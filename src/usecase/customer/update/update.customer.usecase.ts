ae84b7cdf39382d2d26e723a61b4b62a64758d48
Kadu Dutra <carlos.rocha@edu.unipar.br>
2023-07-29 16:13:27 -0300

    aula 10

--- src/usecase/customer/update/update.customer.usecase.ts
+++ src/usecase/customer/update/update.customer.usecase.ts
@@ -1,5 +1,5 @@
 import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository.interface";
-import {InputCreateCustomerDto, OutputCreateCustomerDto} from "./update.customer.dto";
+import {InputUpdateCustomerDto, OutputUpdateCustomerDto} from "./update.customer.dto";
 import CustomerFactory from "../../../domain/customer/factory/customer.factory";
 import Address from "../../../domain/customer/value-object/address";
 
@@ -10,13 +10,12 @@ export default class UpdateCustomerUsecase {
         this.customerRepository = customerRepository;
     }
 
-    async execute(input: InputCreateCustomerDto): Promise<OutputCreateCustomerDto> {
-        const customer = CustomerFactory.createWithAddress(
-            input.name,
-            new Address(input.address.street, input.address.number, input.address.zip, input.address.city)
-        );
+    async execute(input: InputUpdateCustomerDto): Promise<OutputUpdateCustomerDto> {
+        const customer = await this.customerRepository.find(input.id)
+        customer.changeName(input.name);
+        customer.changeAddress(new Address(input.address.street, input.address.number, input.address.zip, input.address.city))
 
-        await this.customerRepository.create(customer);
+        await this.customerRepository.update(customer);
 
         return {
             id: customer.id,
